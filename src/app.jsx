d12a78803c1bd002e5fd559e4525df38cdace25e
SegunA <Segun.Akinyemi@point72.com>
2017-07-17 13:38:20 -0400

    react-bootstrap addition

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Test from './test.jsx'; 
 
-import '../styles/index.scss';
+import '../styles/styles.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div id="container">
         <Test/> 
         <h1>It Works!</h1>
         <p>This React AWESOME BRODY just works including <span className="redBg">module</span> local styles.</p>
--- src/test.jsx
+++ src/test.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import '../styles/index.scss';
 
 export default class Test extends React.Component {
+
     render() {
         return (
-        <div id="container"> 
-            <img src={require('../images/resizedBackgroundHJ.jpg')}/> 
-            <ol>
-                <li>
-                    The first jumper
-                </li>
-                <li>
-                    The second jumper
-                </li>
+            <div id="container">
+                <img src={require('../images/resizedBackgroundHJ.jpg')}/>
+                <ol>
+                    <li>
+                        The first jumper
+                    </li>
+                    <li>
+                        The second jumper
+                    </li>
 
-            </ol>
-        </div> 
+                </ol>
+            </div>
         )
     }
 
- 
 }
